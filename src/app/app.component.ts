8ebb49fe2f34bf986fc011ae6864650da890c062
Ranveer Singh <ranveer08@hotmail.com>
2020-06-18 15:50:20 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'portfolio';
+  loadScripts() {
+    const dynamicScripts = [
+      "../../assets/js/main.js"
+    ];
+    for (let i = 0; i < dynamicScripts.length; i++) {
+      const node = document.createElement('script');
+      node.src = dynamicScripts[i];
+      node.type = 'text/javascript';
+      node.async = false;
+      node.charset = 'utf-8';
+      document.getElementsByTagName('head')[0].appendChild(node);
+    }
+  }
+  constructor() {
+    this.loadScripts();
+}
+
 }
